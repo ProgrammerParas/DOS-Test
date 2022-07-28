afbb12d597fefd0b4ff3c356a6b335cd6c16563b
ProgrammerParas <69015017+ProgrammerParas@users.noreply.github.com>
2022-07-28 22:56:36 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,14 @@
+a, l = input("Enter: ").split()
+b, n = input("Enter: ").split()
+c, n = input("Enter: ").split()
+d, l = input("Enter: ").split()
+
+if n.isnumeric():
+    print("True")
+else:
+    print("False")
+
+if l.isalpha():
+    print("True")
+else:
+    print("False")
